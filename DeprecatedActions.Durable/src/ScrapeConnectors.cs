2fd02ccde5eeac16e0314b1bf1ab6d387c55535b
Phil Cole <filcole@gmail.com>
2021-09-07 14:46:59 +0100

    Simplify code for first blog post

--- DeprecatedActions.Durable/src/ScrapeConnectors.cs
+++ DeprecatedActions.Durable/src/ScrapeConnectors.cs
@@ -14,10 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace DeprecatedActions.Durable
@@ -26,10 +23,6 @@ namespace DeprecatedActions.Durable
     {
         private readonly static Regex sConnectorUniqueName = new Regex(@"^\.\.\/(.*)\/$");
 
-        [OpenApiOperation(operationId: nameof(ScrapeConnectors), tags: new[] { "default" }, Summary = "Get Actions", Description = "Gets the actions and their deprecation status for one or more custom connectors", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
-        [OpenApiRequestBody(contentType: "application/json", typeof(ScrapeConnectorsRequest), Required = true)]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(List<ConnectorInfo>), Summary = "Connector information", Description = "The actions/operations within a connector and the status thereof")]
         [FunctionName(nameof(ScrapeConnectors))]
         public static async Task<IActionResult> ScrapeConnectors(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
@@ -47,96 +40,20 @@ namespace DeprecatedActions.Durable
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
-            // We will NOT use the 'standard' check status response here, because Power Automate requires a status response
-            // return starter.CreateCheckStatusResponse(req, instanceId);
-
-            // Ref: 
-            //  https://powerusers.microsoft.com/t5/Connecting-To-Data/Signalling-error-failure-condition-to-Power-Automate-from-Async/td-p/765893)
-            //  https://docs.microsoft.com/en-us/azure/architecture/patterns/async-request-reply#asyncprocessingworkacceptor-function
-            //  https://pacodelacruzag.wordpress.com/2018/07/10/async-http-apis-with-azure-durable-functions-2/
-            //  https://kendaleiv.com/avoid-exposing-status-uri-from-durable-functions-extension-for-azure-functions-to-untrusted-parties-it-contains-a-secret-key/
-            var checkStatusLocation = GetCheckStatusLocation(req, instanceId);
-
-            ActionResult response = new AcceptedResult(checkStatusLocation, new
-            {
-                id = instanceId
-            }); 
-
-            // To inform the client how long to wait in seconds before checking the status
-            req.HttpContext.Response.Headers.Add("retry-after", "20"); 
-
-            return response;
+            // Return the 'standard' check status response, Accepted 202 status
+            return starter.CreateCheckStatusResponse(req, instanceId);
         }
 
-        private static string GetCheckStatusLocation(HttpRequest req, string instanceId)
-        {
-            return string.Format("{0}://{1}/api/ScrapeConnectorsStatus/instance/{2}", req.Scheme, req.Host, instanceId);
-        }
-
-        /// <summary>
-        /// Http Triggered Function which acts as a wrapper to get the status of a running Durable orchestration instance.
-        /// It enriches the response based on the GetStatusAsync's retruned value
-        /// I'm using Anonymous Authorisation Level for demonstration purposes. You should use a more secure approach. 
-        /// </summary>
-        /// <param name="req"></param>
-        /// <param name="orchestrationClient"></param>
-        /// <param name="instanceId"></param>
-        /// <param name="logger"></param>
-        /// <returns></returns>
-        [OpenApiOperation(operationId: nameof(ScrapeConnectorsStatus), tags: new[] { "default" }, Summary = "Get Status of Durable Function", Description = "Gets the status of a durable function", Visibility = OpenApiVisibilityType.Internal)]
-        [OpenApiParameter(name: "instanceId", In = ParameterLocation.Path, Required = true, Type = typeof(string))]
-        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK, Description = "default")]
-        [FunctionName(nameof(ScrapeConnectorsStatus))]
-        public static async Task<IActionResult> ScrapeConnectorsStatus (
-            [HttpTrigger(AuthorizationLevel.Anonymous, methods: "get", Route = "ScrapeConnectorsStatus/instance/{instanceId}")] HttpRequest req,
-            [DurableClient] IDurableOrchestrationClient orchestrationClient,
-            string instanceId,
-            ILogger logger)
-        {
-            // Get the built-in status of the orchestration instance. This status is managed by the Durable Functions Extension. 
-            var status = await orchestrationClient.GetStatusAsync(instanceId);
-            if (status != null)
-            {
-                // Get the custom status of the orchestration intance. This status is set by our code. 
-                // This can be any serialisable object. In this case, just a string. 
-                string customStatus = (string)status.CustomStatus;
-                if (status.RuntimeStatus == OrchestrationRuntimeStatus.Running || status.RuntimeStatus == OrchestrationRuntimeStatus.Pending)
-                {
-                    //The URL (location header) is prepared so the client know where to get the status later. 
-                    //string checkStatusLocation = string.Format("{0}://{1}/api/ScrapeConnectorsStatus/{2}", req.Scheme, req.Host, instanceId);
-                    string checkStatusLocation = GetCheckStatusLocation(req, instanceId);
-
-                    // Create an Http Response with Status Accepted (202) to let the client know that the original request might not be acted up on yet
-                    ActionResult response = new AcceptedResult(checkStatusLocation, new
-                    {
-                        id = instanceId
-                    }); 
-
-                    // To inform the client how long to wait before checking the status. 
-                    req.HttpContext.Response.Headers.Add("retry-after", "20"); 
-                    return response;
-                }
-                else if (status.RuntimeStatus == OrchestrationRuntimeStatus.Completed)
-                {
-                    return new OkObjectResult(status.Output);
-                }
-                else if (status.RuntimeStatus == OrchestrationRuntimeStatus.Failed)
-                {
-                    return new BadRequestObjectResult(status.Output);
-                }
-                throw new Exception($"Unexpected RuntimeStatus: {status.RuntimeStatus}");
-            }
-
-            // If status is null, then instance has not been found. Create and return an Http Response with status NotFound (404). 
-            return new NotFoundObjectResult($"Whoops! Something went wrong. Please check if your submission Id is correct. Submission '{instanceId}' not found.");
-        }
+        //
+        // Durable Function Orchestrator and Actions 
+        //
 
         [FunctionName("ScrapeConnectorsOrchestration")]
         public static async Task<List<ConnectorInfo>> RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
         {
             // Scrape the connectors from the main page
-            var connectors = await context.CallActivityAsync<List<ConnectorInfo>>(nameof(ScrapeConnectorsOrchestration_ScrapeConnectors), "bob");
+            var connectors = await context.CallActivityAsync<List<ConnectorInfo>>(nameof(ScrapeConnectorsOrchestration_ScrapeConnectors), null);
 
             // Get the request parameters passed into the orchestrator
             var request = context.GetInput<ScrapeConnectorsRequest>();
@@ -145,11 +62,6 @@ namespace DeprecatedActions.Durable
             {
                 // Filter connectors found to just the selectedConnectors required
                 connectors = connectors.Where(c => request.SelectedConnectors.Contains(c.UniqueName)).ToList();
-
-                if (connectors.Count < request.SelectedConnectors.Count())
-                {
-                    throw new Exception("Not all connectors found");
-                }
             }
 
             // Create a task for each connector to get the actions from their own docs page
@@ -159,13 +71,14 @@ namespace DeprecatedActions.Durable
                 tasks.Add(context.CallActivityAsync<ConnectorInfo>(nameof(ScrapeConnectorsOrchestration_GetConnectorInfo), connector));
             }
 
-            // Wait for all the tasks to finish
+            // Wait for all the tasks to finish (Fan-In)
             await Task.WhenAll(tasks);
 
-            // Put the results together
-            var result = tasks.Select(t => t.Result);
-
-            return result.OrderBy(r => r.UniqueName).ToList();
+            // Put the results together and return them
+            return tasks
+                .Select(t => t.Result)
+                .OrderBy(r => r.UniqueName)
+                .ToList();
         }
 
         // Scrape Connection Reference page to get the names of all the connectors
